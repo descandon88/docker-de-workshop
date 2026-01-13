a41ef9f28b36491a76414292a3cf037f2130702c
David Escandón <david.escandon.v@gmail.com>
2026-01-13 00:03:49 -0300

    commit for remote repository

---
+++ README.md
@@ -0,0 +1,8 @@
+1: 35 min 01/12/2026
+
+44 min in 10/12/2025
+
+42 minute in 10/12/2025
+
+30 minute in 09/12/2025
+https://www.youtube.com/watch?v=lP8xXebHmuE
---
+++ test/script.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+current_directory = Path.cwd()
+current_file = Path(__file__).name
+
+print(f"Files in {current_directory}")
+
+for filepath in current_directory.iterdir():
+    if filepath.name == current_directory:
+        continue
+
+    print(f" - {filepath.name}")
+
+    if filepath.is_file():
+        content = filepath.read_text(encoding = 'utf-8')
+        print(f"   Content: {content}")
+                  
